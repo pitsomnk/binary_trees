3573d80f0bea7df49ef3b6fa1ce84fb93eb7ae08
Pitso Manyike <pitsomnk@gmail.com>
2024-07-25 05:52:00 -0700

    binary_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,19 +10,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode;
+	binary_tree_t *new_node;
 
-	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-
-	if (newNode == NULL)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
-	newNode->parent = parent;
-	newNode->n = value;
-	newNode->left = NULL;
-	newNode->right = NULL;
-
-	return (newNode);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
